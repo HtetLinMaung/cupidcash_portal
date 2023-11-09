88101e8668d6963d883354e6161c10bcfc8a9275
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-11-10 06:19:37 +0630

    update

--- app/page.js
+++ app/page.js
@@ -7,7 +7,12 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/dashboard");
+    const token = localStorage.getItem("cupidcash_token");
+    if (token) {
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
   }, [router]);
 
   return <div></div>;
