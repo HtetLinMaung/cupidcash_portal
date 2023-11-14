2cd80e011ef1c9fe3e301abdcd687312a1b99164
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-11-14 23:59:33 +0630

    update

--- components/ProviderContainer.jsx
+++ components/ProviderContainer.jsx
@@ -2,13 +2,16 @@ import CategoryProvider from "@/providers/CategoryProvider";
 import PaymentProvider from "@/providers/PaymentProvider";
 import AppProvider from "@/providers/AppProvider";
 import UserProvider from "@/providers/UserProvider";
+import ItemProvider from "@/providers/ItemProvider";
 
 export default function ProviderContainer({ children }) {
   return (
     <AppProvider>
       <UserProvider>
         <CategoryProvider>
-          <PaymentProvider>{children}</PaymentProvider>
+          <PaymentProvider>
+            <ItemProvider>{children}</ItemProvider>
+          </PaymentProvider>
         </CategoryProvider>
       </UserProvider>
     </AppProvider>
---
+++ providers/ItemProvider.jsx
@@ -0,0 +1,23 @@
+import useData from "@/hooks/use-data";
+import { createContext } from "react";
+
+const initState = {
+  shops: [],
+  items: [],
+  categories: [],
+  search: "",
+  page: 1,
+  perPage: 10,
+  pageCounts: 0,
+  total: 0,
+};
+
+export const itemContext = createContext(initState);
+
+export default function ItemProvider({ children }) {
+  return (
+    <itemContext.Provider value={useData(initState)}>
+      {children}
+    </itemContext.Provider>
+  );
+}
